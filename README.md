b7c65cd7d32314a9a616a675db5a25641a36c02c
TWGraceChen <grace840328@gmail.com>
2022-12-19 23:38:22 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # LineBot
+
+## 建立與佈署Line Bot
+1. 在Line Developers建立Provider和Channel
+https://developers.line.biz/en/
+2. 完成基本設置，取得channel_token和channel_secret
+參考：https://ithelp.ithome.com.tw/articles/10229943
+3. 啟動go http service
+4. 使用ngrok將local的port forword出去
+-- 安裝ngrok：https://ngrok.com/download
+-- 註冊登入後，取得token：https://dashboard.ngrok.com/get-started/your-authtoken
+-- 在local新增token：`ngrok config add-authtoken <AUTH_TOKEN>`
+-- 啟動ngrok client：`ngrok http <PORT>`
+-- 取得URL
+5. 回到Line Developers填入webhook URL
+6. 完成
+
+## TO DO
+- 把token和secret拉到外部config [OK]
+- port用flag來指定 [OK]
+- push各種type的message
+- logging
+- 把data存到db
+- 啟動python的gRPC server，讓go可以使用python的function。
+- dockerize
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"flag"
+	"github.com/spf13/viper"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+var (
+	port = flag.Int("p", 5000, "port number.")
+)
+
+func main() {
+	flag.Parse()
+
+	// read config file
+	v := viper.New()
+	v.SetConfigName("config")
+    v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	err := v.ReadInConfig()
+    if err != nil {
+        log.Fatal("[Error] Loading config failed: ", err)
+    }
+	channelSecret := v.GetString("linebot.channel_secret")
+	channelAccessToken := v.GetString("linebot.channel_access_token")
+	
+
+	// create a linebot
+	bot, err := linebot.New(channelSecret,channelAccessToken,)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	url := fmt.Sprintf(":%v",*port)
+	log.Println("listing:"+url)
+	if err := http.ListenAndServe(url, nil); err != nil {
+		log.Fatal(err)
+	}
+}
