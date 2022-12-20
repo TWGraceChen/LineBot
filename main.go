9ef8f2e9d920c673b51254d00ff2dd705adefc6d
TWGraceChen <grace840328@gmail.com>
2022-12-21 00:48:49 +0800

    add meaadge type test

--- main.go
+++ main.go
@@ -18,17 +18,12 @@ var (
 func getUserProfile(e *linebot.Event) {
 	switch e.Source.Type {
 	case linebot.EventSourceTypeUser:
-		log.Println("from user:" + e.Source.UserID)
 		userProfile, err := bot.GetProfile(e.Source.UserID).Do()
 		if err != nil {
 			log.Println("Get user profile fail:", err)
 		} else {
-			log.Println("Get user profile success!!")
-			log.Println("UserID:" + userProfile.UserID)
-			log.Println("DisplayName:" + userProfile.DisplayName)
-			log.Println("PictureURL:" + userProfile.PictureURL)
-			log.Println("StatusMessage:" + userProfile.StatusMessage)
-			log.Println("Language:" + userProfile.Language)
+			log.Printf("UserID:%v, DisplayName:%v, PictureURL:%v, StatusMessage:%v, Language:%v",
+				userProfile.UserID, userProfile.DisplayName, userProfile.PictureURL, userProfile.StatusMessage, userProfile.Language)
 		}
 	case linebot.EventSourceTypeGroup:
 		log.Println("from group:" + e.Source.GroupID)
@@ -71,17 +66,41 @@ func main() {
 		}
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
-
+				// read user profile
 				getUserProfile(event)
 
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					// reply different type of message base on request message content
+					var reply linebot.SendingMessage
+					switch message.Text {
+					case "1":
+						reply = linebot.NewTextMessage("text message")
+					case "2":
+						reply = linebot.NewStickerMessage("446", "1988")
+					case "3":
+						reply = linebot.NewImageMessage("https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-1001682.jpg&fm=jpg", "https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-1001682.jpg&fm=jpg")
+					case "4":
+						reply = linebot.NewVideoMessage("https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4", "https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4")
+					case "5":
+						reply = linebot.NewAudioMessage("https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3", 10)
+					case "6":
+						reply = linebot.NewLocationMessage("My location", "台北市中正區忠孝東路一段9號4樓", 25.0450205, 121.5214747)
+					default:
+						reply = linebot.NewTextMessage("1:Text\n2:Sticker\n3:Image\n4:Video\n5:Audio\n6:Location")
+					}
+
+					if _, err = bot.ReplyMessage(event.ReplyToken, reply).Do(); err != nil {
 						log.Print(err)
 					}
 				case *linebot.StickerMessage:
 					replyMessage := fmt.Sprintf(
 						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.ImageMessage:
+					replyMessage := fmt.Sprintf("image ID is %s", message.ID)
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 						log.Print(err)
 					}
