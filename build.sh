cb7aaec2f3d9003d0d29e21695c05b46dc65f2eb
TWGraceChen <grace840328@gmail.com>
2023-01-20 14:45:10 +0800

    update go grpc output path

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 python3 -m grpc_tools.protoc -I ./src --python_out=./src/service --pyi_out=./src/service --grpc_python_out=./src/service service.proto
 export PATH="$PATH:$(go env GOPATH)/bin"
-protoc --go_out=./src/service_client --go_opt=paths=source_relative --go-grpc_out=./src/service_client --go-grpc_opt=paths=source_relative ./src/service.proto
+protoc -I ./src --go_out=./src/service_client --go_opt=paths=source_relative --go-grpc_out=./src/service_client --go-grpc_opt=paths=source_relative service.proto
 cd src;go build -o ../linebot
