151e726401eba416bd5c98d78dac1048314e3341
TWGraceChen <grace840328@gmail.com>
2023-01-23 03:31:51 +0800

    add ppt.py and lyric.py

--- src/service/lyric.py
+++ src/service/lyric.py
@@ -1,9 +1,39 @@
 # -*- coding: utf-8 -*-
+import requests
+import urllib.parse
+from bs4 import BeautifulSoup
 
 
 def searchLyric(name):
+    site = "https://www.taiwanbible.com"
+    
+    # search id
+    path = "/web/search.jsp"
+    url = site + path
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'JSESSIONID=35454E79B5902585014596FC3BC4C116'
+    }
+    payload='area=lyrics&keyword=' + urllib.parse.quote(name)
+    r = requests.request("POST", url,headers=headers, data=payload)
+    soup = BeautifulSoup(r.text,"lxml")
+    searchResultTable = soup.find_all("table",class_="bordered")[1].find_all('tr')
+
+    if len(searchResultTable) < 2:
+        return ""
+    else:
+        path = searchResultTable[1].find_all('td')[1].find("a")['href']  # skip header and get first result
+    
+
+    # get lyric
+    url = site + path
+    r = requests.request("GET",url,headers=headers)
+    soup = BeautifulSoup(r.text,"html.parser")
+
+    print(soup.find_all("span",style="word-break:break-all;"))
     lyric = name+"'s lyric showed below."
     return lyric
 
 if __name__ == '__main__':
-    print(searchLyric("songname"))
+    #print(searchLyric("我要歌頌你的力量"))
+    
---
+++ src/service/ppt.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from pptx import Presentation
+
+
+def makeppt(name,lyric):
+    f = open('./template.pptx',"rb")
+    prs = Presentation(f)
+    
+    pages = lyric.split("\n\n")
+    title_slide_layout = prs.slide_layouts[1] 
+    for l in pages:
+        slide = prs.slides.add_slide(title_slide_layout)
+        content = slide.placeholders[1]
+        content.text = l
+        title = slide.placeholders[0]
+        title.text = name
+    
+    
+    
+    prs.save("pythonppt.pptx")
+    f.close()
+    
+
+
+
+if __name__ == '__main__':
+    name = "我要歌頌你的力量"
+    lyric = '''你救我脫離死亡 你救我脫離驚惶
+在敵人面前 你為我擺設筵席
+你恩典隨時夠用 你慈愛無盡無窮
+在困難中 我要這樣說
+
+就算是四面受敵 就算是暴風暴雨
+當我要倒下 你卻伸手扶持我
+你已將莫大能力 顯明在我的心裡
+藉著信心 勇敢向前走
+
+所以我要歌頌你的力量
+早晨我要高唱你的慈愛
+因為你作過我的高台
+在急難中作過我的避難所
+
+所以我要歌頌你的力量
+早晨我要高唱你的慈愛
+因為你是我的高台
+是賜恩於我的神'''
+    makeppt(name,lyric)
+    
